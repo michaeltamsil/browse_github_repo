f9bcfdd2b5d80382608c2cbfd27b6b5e04ca02f9
michael tamsil <michaeltamsil@192.168.1.5>
2021-12-01 11:52:21 +0700

    impement NavigationContainer

--- App.js
+++ App.js
@@ -8,12 +8,21 @@
 
 import React from 'react';
 import type {Node} from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Login from './src/Components/Pages/Login';
+import Login from "./src/Components/Pages/Login";
 
+const Stack = createNativeStackNavigator();
 
 const App: () => Node = () => {
-  return (<Login/>)
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login to Github" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default App;
